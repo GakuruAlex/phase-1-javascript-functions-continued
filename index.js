10155568a39a840a2443960a8dce4c9b03c7f957
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-16 11:19:11 +0300

    commit tree 558c6da890194b035daf26cdf1f3439380c95498

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun (activity=`roller-skate`){
+
+const message =`This Saturday, I want to ${activity}!`;
+
+return message;
+
+}
+
+function mondayWork(activity=`go to the office`){
+
+    const message =`This Monday, I will ${activity}.`;
+
+    return message;
+
+}
+function wrapAdjective(symbolName) {
+
+  const result = function  emphatic(msg) {
+
+    return `You are ${symbolName}${msg}${symbolName}!`;
+  };
+  return result;
+}
+
+console.log(wrapAdjective())
